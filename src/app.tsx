7716d7471548b5d91e0f2e0f20997d628c963304
ArmandoRJr <59779955+ArmandoRJr@users.noreply.github.com>
2023-06-11 11:51:56 -0500

    Test hash router

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,21 @@
 import { RouterProvider } from 'react-router'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter } from 'react-router-dom'
 import ErrorPage from './error-page'
 import EcoloCrimes from './routes/ecolo-crimes'
 import Root from './routes/root'
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '/ecolo-crimes',
-          element: <EcoloCrimes />,
-        },
-      ],
-    },
-  ],
-  { basename: '/puyo-gateway' }
-)
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/ecolo-crimes',
+        element: <EcoloCrimes />,
+      },
+    ],
+  },
+])
 
 const App = () => {
   return (
